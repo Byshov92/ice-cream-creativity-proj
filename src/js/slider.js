587b67b61d1c8cfe466c3dcce4a57d22b5026c89
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-06-08 16:14:28 +0300

    Сделал скрипт и немного стилизовал

---
+++ src/js/slider.js
@@ -0,0 +1,14 @@
+$('.customer-reviews').slick({
+  dots: true,
+  arrows: true,
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 1,
+  // slidesToScroll: 1,
+  autoplaySpeed: 200000,
+  centerMode: false,
+  // variableWidth: true,
+  nextArrow: '.next',
+  prevArrow: '.prev',
+  lazyLoad: 'ondemand',
+});
